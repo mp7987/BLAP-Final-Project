88a966e5f9861911c70e152af145c06f4bf15a1a
mp7987 <mpat707@gmail.com>
2026-04-24 10:36:47 -0400

    Project Complete

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 // API: http://www.omdbapi.com/?apikey=[yourkey]&
 // API Key: a4bd795f
 // http://www.omdbapi.com/?apikey=[a4bd795f]&
+
+let searchTerm;
+
+document.querySelector(".input").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    searchMovies();
+  }
+});
+
+const moviesContainer = document.querySelector(".movies-container");
+async function fetchMovies() {
+  const res = await fetch(
+    `http://www.omdbapi.com/?apikey=a4bd795f&s=${searchTerm || "batman"}`
+  );
+  const { Search: movies } = await res.json();
+  moviesContainer.innerHTML = movies
+    .map((movie) => {
+      return `
+        <div class="movie">
+          <figure>
+            <img src="${movie.Poster}" alt="">
+          </figure>
+        <div>
+          <h2>
+            ${movie.Title}
+          </h2>
+          <p>${movie.Year}</p>
+        </div>
+      </div>
+      `;
+    })
+    .join("");
+}
+fetchMovies();
+
+function setSearchTerm(event) {
+  searchTerm = event.target.value;
+  fetchMovies();
+}
